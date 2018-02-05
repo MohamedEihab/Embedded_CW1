3453bee813072c7110924d4c49d3c8fcf4acd3b4
MohamedEihab <mohammedeihabmail+github@gmail.com>
2018-02-05 15:16:15 +0000

    Json dump works

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from machine import I2C, Pin
 import time
+import machine
+import network
+import ujson
+
+pin = machine.Pin(2, machine.Pin.OUT) #Playing around with LEDs and machine pins
 
 i2cport =I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 i2cport.writeto_mem(24, 0x20, bytearray([0x7F]))
+
+gyro = {'x_dir' : 0, 'y_dir' : 0, 'z_dir' : 0}
+
 while(True):
+    pin.on() #LED ON
+
 
     X_L = i2cport.readfrom_mem(24, 0x28, 1)
     X_H = i2cport.readfrom_mem(24, 0x29, 1)
@@ -16,6 +26,14 @@ while(True):
     y_comb = int.from_bytes(Y_H,'big')
     z_comb = int.from_bytes(Z_H,'big')
 
-    print('%d %d %d - TEST2'%(x_comb,y_comb,z_comb))
+    gyro['x_dir'] = x_comb;
+    gyro['y_dir'] = y_comb;
+    gyro['z_dir'] = z_comb;
+
+    #print('%d %d %d - TEST2'%(gyro['x_dir'],gyro['y_dir'],gyro['z_dir']))
+    print(ujson.dumps(gyro))
+
     time.sleep(0.2)
+
+    pin.off() #LED OFF
 
