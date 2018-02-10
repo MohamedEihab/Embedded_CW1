fa0a1fa2a8a6d4120955f24777d918bf76b686e6
MohamedEihab <mohammedeihabmail+github@gmail.com>
2018-02-10 17:22:13 +0000

    lux sensor working

--- main.py
+++ main.py
@@ -1,62 +1,23 @@
 from machine import I2C, Pin
 import time
-import machine
-import network
-import ujson
-from umqtt.simple import MQTTClient
-
-
-
-
-pin = machine.Pin(2, machine.Pin.OUT) #Playing around with LEDs and machine pins
 
 i2cport =I2C(scl=Pin(5), sda=Pin(4), freq=100000)
-i2cport.writeto_mem(24, 0x20, bytearray([0x7F]))
+i2cport.writeto_mem(57, 0, b'\0x03')
 
-gyro = {'x_dir' : 0, 'y_dir' : 0, 'z_dir' : 0}
-
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False) #disable access point
-
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('EEERover', 'exhibition')
-
-# ap_if.ifconfig() # access point data
-
-client = MQTTClient('Mohamed_Machine', '192.168.0.10')
-
-while(not sta_if.isconnected()):
-    time.sleep(0.1)
-
-if (sta_if.isconnected()):
-    client.connect()
 
 while(True):
-    pin.on() #LED ON
+#    pin.on() #LED ON
 
-    X_L = i2cport.readfrom_mem(24, 0x28, 1)
-    X_H = i2cport.readfrom_mem(24, 0x29, 1)
-    Y_L = i2cport.readfrom_mem(24, 0x2A, 1)
-    Y_H = i2cport.readfrom_mem(24,0x2B, 1)
-    Z_L = i2cport.readfrom_mem(24, 0x2C, 1)
-    Z_H = i2cport.readfrom_mem(24,0x2D,1)
-    
-    print("Is connected ", sta_if.isconnected())
+    data0low = int.from_bytes(i2cport.readfrom_mem(57,0xC,1),'big')
+    data0high = int.from_bytes(i2cport.readfrom_mem(57,0xD,1),'big')
+    channel0 = data0high*256 + data0low
+    data1low = i2cport.readfrom_mem(57,0xE,1)
+    data1lhigh = i2cport.readfrom_mem(57,0xF,1)
 
-    x_comb = int.from_bytes(X_H,'big')
-    y_comb = int.from_bytes(Y_H,'big')
-    z_comb = int.from_bytes(Z_H,'big')
-
-    gyro['x_dir'] = x_comb;
-    gyro['y_dir'] = y_comb;
-    gyro['z_dir'] = z_comb;
-
-    #print('%d %d %d - TEST2'%(gyro['x_dir'],gyro['y_dir'],gyro['z_dir']))
-    print(ujson.dumps(gyro))
-    #client.publish("TESTDATA", bytes(ujson.dumps(gyro)), 'utf-8')
-    client.publish(b"foo_topic", b"hello")
+    print("%d; %d; %d; %d", data0low,data0high,data1low,data1lhigh)
+    print(channel0)
     time.sleep(0.2)
 
-    pin.off() #LED OFF
+#    pin.off() #LED OFF
 
+i2cport.writeto_mem(57, 0x00, bytearray([0x03]))
