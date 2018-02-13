7f04bea9af2bfd3a55b2dff7904f70805aecc54b
MohamedEihab <mohammedeihabmail+github@gmail.com>
2018-02-13 16:15:53 +0000

    combining everything together

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ from machine import I2C, Pin
 import time
 import ujson
 
+
 # CONSTANTS
 SLAVE_ADDRESS = 0x39
 
+i2cport_acc = I2C(scl = Pin(2), sda = Pin(16), freq = 100000)
 i2cport =I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+
+
 i2cport.writeto_mem(SLAVE_ADDRESS, 0, b'\0x03')
 
+#temp setup
+i2cport_acc.writeto_mem(24, 0x1F, b'\0xC0')
+i2cport_acc.writeto_mem(24, 0x23, b'\0x80')
+i2cport_acc.writeto_mem(24, 0x20, bytearray([0x7F]))
+
 lux_sensor = {'channel_0': 0, 'channel_1' : 0}
+temp_sensor = {'temperature' : 0}
+gyro = {'x_dir' : 0, 'y_dir' : 0, 'z_dir' : 0}
+
 lux_input = [201]
 iterator = 0;
 
 def process_input_data():
+
+    #lux sensor data
     data0low = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8C,1),'big')
     data0high = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8D,1),'big')
 
@@ -24,6 +38,29 @@ def process_input_data():
 
     lux_sensor['channel_0'] = channel0
     lux_sensor['channel_1'] = channel1
+
+    #temperature data
+  #  temp_low = int.from_bytes(i2cport_acc.readfrom_mem(24,0x0C,1),'big')
+  #  temp_high = int.from_bytes(i2cport_acc.readfrom_mem(24,0x0D,1),'big')
+
+  #  temp_sensor['temperature'] = 256*temp_high + temp_low;
+
+    #acceler data
+    X_L = i2cport_acc.readfrom_mem(24, 0x28, 1)
+    X_H = i2cport_acc.readfrom_mem(24, 0x29, 1)
+    Y_L = i2cport_acc.readfrom_mem(24, 0x2A, 1)
+    Y_H = i2cport_acc.readfrom_mem(24,0x2B, 1)
+    Z_L = i2cport_acc.readfrom_mem(24, 0x2C, 1)
+    Z_H = i2cport_acc.readfrom_mem(24,0x2D,1)
+
+    x_comb = int.from_bytes(X_H,'big')
+    y_comb = int.from_bytes(Y_H,'big')
+    z_comb = int.from_bytes(Z_H,'big')
+
+    gyro['x_dir'] = x_comb;
+    gyro['y_dir'] = y_comb;
+    gyro['z_dir'] = z_comb;
+
     return;
 
 
@@ -40,8 +77,9 @@ while(True):
 
     lux_input.append(lux_sensor['channel_0'])
 
-
     print(ujson.dumps(lux_sensor))
+    print(ujson.dumps(gyro))
+   # print(ujson.dumps(temp_sensor))
 
     time.sleep(0.2)
 
