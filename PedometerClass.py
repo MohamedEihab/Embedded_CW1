02529c9f37a2118855c39a7a41a33742c0d9e263
Shrey Bagga <Shreyus@dyn1201-179.wlan.ic.ac.uk>
2018-02-15 12:56:49 +0000

    Comments and bug fix

--- PedometerClass.py
+++ PedometerClass.py
@@ -10,20 +10,28 @@ class PedometerClass():
 	iterator = 0
 	walk = 0
 	gesture = 0
-	
+
+#action function increments walk counter for pedometer
+#IntelliBand strapped to wrist, so change in sign in x direction used
+#to detect each step	
 	def action(self):
 		if(self.iterator!=0):
 			i = self.iterator
 			if((self.x_acc[i-1] <0 and self.x_acc[i]>0) or (self.x_acc[i-1]>0 and self.x_acc[i]<0)):
 				self.walk = self.walk +1
 
+#Records a gesture and stores accelerometer data (trained for that gesture)
+#If user repeats gesture (with some allowance)
+#then recognise that the gesture was performed
 	def RecordGesture(self, Done):
 		if(Done==False):
 			self.GestureY[self.iterator] = self.y_acc[self.iterator]
 			self.GestureZ[self.iterator] = self.z_acc[self.iterator]
 			if self.iterator ==199:
-				print("IT IS DONE")
+				print("GESTURE STORED")
 				Done = True
+		#Gesture has been stored 
+		# If user repeats gesture, recognise and print that gesture was performed
 		if(Done == True):
 			i = self.iterator
 			if((self.y_acc[i] <= self.GestureY[i] + 2.0) or (self.y_acc[i] >= self.GestureY[i] - 2.0)):
@@ -32,7 +40,8 @@ class PedometerClass():
 					if(self.gesture >= 5):
 						while True:
 							print("YOU JUST PERFORMED A GESTURE")
-
+	#Store the last constant number of values to be processed by other functions
+	#Overwrite old stored data after each loop
 	def process_raw_data(self, x_in, y_in, z_in):
 		if (self.iterator >= 100):
 			self.iterator = 0
@@ -46,13 +55,13 @@ class PedometerClass():
 			self.action()
 			self.RecordGesture(self.Done)
 			self.iterator += 1
-	
+	#Recognises if a certain number of steps have been done
 	def WalkTenSteps(self):
 		if(self.walk == 20):
 			return True
 		else:
 			return False	
-	
+	#return number of steps
 	def getSteps(self):
 		return self.walk
 
--- client.py
+++ client.py
@@ -3,17 +3,22 @@ import time
 import json
 import matplotlib.pyplot as plt
 
+#Empty initialised lists
 stepsTaken = []
 heartRate = []
 timeTaken = []
 
+#Plot graphs with hearbeat sensor, pedometer sensor data
+#and time taken
 def plot(a, b, xaxis, yaxis):
     plt.figure()
     plt.plot(a,b)
     plt.ylabel(yaxis)
     plt.xlabel(xaxis)
 
-
+#Call back function tied to the client
+#Extracts payload from MQTT JSON output
+#Uses REGEX to extract numbers from heartrate and steps taken
 def on_message(client, userdata, message):
     message_rec = str(message.payload.decode("utf-8"))
 
@@ -30,19 +35,25 @@ def on_message(client, userdata, message):
 
     print("Heart Rate: %s"%heart_rate) 
 
+#Appends to lists in order to use in plotting function
     stepsTaken.append(int(steps_taken))
     heartRate.append(int(heart_rate))
     timeTaken.append(int(time.time() - startTime))
 
     #print("message topic=",message.topic)
-    #return  "\'" + message_rec + "\'"
+
+#Store time at start of data being recieved    
 startTimeEn = True
 startTime = None
 
+#Set up client 
 client = mqtt.Client("ShreyBook")
+#Link function to callback
 client.on_message = on_message
+#Input Broker IP
 client.connect("192.168.43.167")
 
+#Start client loop
 client.loop_start()
 if(startTimeEn):
     startTime = time.time()
@@ -50,9 +61,14 @@ if(startTimeEn):
 client.subscribe("topic/state")
 (client.on_message)
 
-
+#End client loop after 30 seconds - configurable
+#Plots graphs after this time
 time.sleep(30) # wait
 client.loop_stop()
+
+#Graph plots only if length of a and b in plot(a,b) are the same
+#Plot immediately if these two arrays have the same length
+#Otherwise modify length of the longer array to match the shorter one 
 if(len(heartRate)==len(stepsTaken) and len(heartRate)==len(timeTaken)):
     plot(stepsTaken,heartRate, 'Steps Taken', 'Heart rate /bpm')
     plot(timeTaken, stepsTaken, 'Time taken /s', 'Steps Taken')
