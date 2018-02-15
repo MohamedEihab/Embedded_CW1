67f5ba5c2ad270660f2ebea8cbe5582735498f18
MohamedEihab <mohammedeihabmail+github@gmail.com>
2018-02-15 12:03:39 +0000

    commented and fixed

--- main.py
+++ main.py
@@ -14,30 +14,31 @@ SLAVE_ADDRESS = 0x39
 TWOG = 16380
 Stop = False
 
+# I2Cport to connect to the accelerometer
 i2cport_acc = I2C(scl = Pin(2), sda = Pin(16), freq = 100000)
+
+# I2Cport to connect to Luxsensor
 i2cport =I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 
+# Turning on the Lux Sensor
 i2cport.writeto_mem(SLAVE_ADDRESS, 0, b'\0x03')
 
-#temp setup
+#Accelerometer setup
 i2cport_acc.writeto_mem(24, 0x1F, b'\0xC0')
 i2cport_acc.writeto_mem(24, 0x23, b'\0x80')
 i2cport_acc.writeto_mem(24, 0x20, bytearray([0x4F]))
 
 #LED Pin
-
 LED_out = machine.Pin(15, machine.Pin.OUT)
 
-lux_sensor = {'channel_0': 0, 'channel_1' : 0}
-temp_sensor = {'temperature' : 0}
-gyro = {'x_dir' : 0, 'y_dir' : 0, 'z_dir' : 0}
+# Tuples for sensor data
+lux_sensor = {'channel_0': 0, 'channel_1' : 0} # Packaged Sensor Data
+temp_sensor = {'temperature' : 0} # Packaged Temperature Data
+gyro = {'x_dir' : 0, 'y_dir' : 0, 'z_dir' : 0} # Packaged Gyro
 
-broker_package = {'Steps_Taken' : 0, 'Heart_Rate' : 0}
+broker_package = {'Steps_Taken' : 0, 'Heart_Rate' : 0} # Server message package
 
-lux_input = [201]
-iterator = 0
-
-
+# Signs the input x
 def to_signed(x):
     xor_bits = 0xFFFF
     if x>=0x7FFF:
@@ -47,19 +48,23 @@ def to_signed(x):
         y=x
     return y
 
-
+# Takes the raw data from the sensors
+# Mutates them into a suitable form, changing the data into measurable units
+# Inputs the data into the packages
 def process_input_data():
 
-    #lux sensor data
+    #lux input sensor data
     data0low = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8C,1),'big')
     data0high = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8D,1),'big')
 
     data1low = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8E,1),'big')
     data1lhigh = int.from_bytes(i2cport.readfrom_mem(SLAVE_ADDRESS,0x8F,1),'big')
 
+    # Two data channel from the lux combining the high and low data
     channel0 = data0high*256 + data0low
     channel1 = data1lhigh*256 + data1low
 
+    # Inputting the data into the package
     lux_sensor['channel_0'] = channel0
     lux_sensor['channel_1'] = channel1
 
@@ -69,7 +74,7 @@ def process_input_data():
 
   #  temp_sensor['temperature'] = 256*temp_high + temp_low
 
-    #acceler data
+    # Accelerometer data
     X_L = i2cport_acc.readfrom_mem(24, 0x28, 1)
     X_H = i2cport_acc.readfrom_mem(24, 0x29, 1)
     Y_L = i2cport_acc.readfrom_mem(24, 0x2A, 1)
@@ -77,50 +82,54 @@ def process_input_data():
     Z_L = i2cport_acc.readfrom_mem(24, 0x2C, 1)
     Z_H = i2cport_acc.readfrom_mem(24,0x2D,1)
 
+    # Converts data into integer
     x_comb = int.from_bytes(X_H,'big')<<8
     y_comb = int.from_bytes(Y_H,'big')<<8
     z_comb = int.from_bytes(Z_H,'big')<<8
+    
+    # Signs the data
     x_combf = float(to_signed(x_comb))
     y_combf = float(to_signed(y_comb))
     z_combf = float(to_signed(z_comb))
-    x_combf = x_combf*10/TWOG #Convert to m/s^2
+
+    # Convert to m/s^2
+    x_combf = x_combf*10/TWOG 
     y_combf = y_combf*10/TWOG
     z_combf = z_combf*10/TWOG
 
+    # Inputs the data into the accelerometer package
     gyro['x_dir'] = x_combf
     gyro['y_dir'] = y_combf
     gyro['z_dir'] = z_combf
 
     return
 
-counter = 0;
-time = 0;
+counter = 0 # Keeps track of the time
+time = 0 # Total elapsed time
 delay = 20 #delay in ms
-simulated_bpm = 70
-duty_time = (60/simulated_bpm)/2
-duty_counter = duty_time / (delay/1000)
-led_switch = 0;
-buffered_val = simulated_bpm
+simulated_bpm = 70 # The simulated BPM for the LED
+duty_time = (60/simulated_bpm)/2 # How long the LED should be on and off for the simulated BPM
+duty_counter = duty_time / (delay/1000) # How many delays should we wait before flipping the LED
+led_switch = 0; # Turns on the LED 
 
-HeartRateClass = HeartRateProcessor.HeartRateProcessorClass(delay, buffered_val)
+# Instances of the classes used 
+HeartRateClass = HeartRateProcessor.HeartRateProcessorClass(delay)
 PedometerInstance= PedometerClass.PedometerClass()
 MQTTClientInstance = MQTTClientClass.MQTTClientClass()
 
+# The main loop for running the device
 while(True):
-   # print(ujson.dumps(lux_sensor['channel_0']), ujson.dumps(lux_sensor['channel_1']))
-    process_input_data()
-    HeartRateClass.process_raw_lux(lux_sensor['channel_1'])
-    PedometerInstance.process_raw_data(gyro['x_dir'], gyro['y_dir'], gyro['z_dir'])
 
-    broker_package['Steps_Taken'] = PedometerInstance.getSteps()
-    broker_package['Heart_Rate'] = HeartRateClass.getHeartRate()
-    MQTTClientInstance.publish_data('topic/state', bytes(ujson.dumps(broker_package), 'utf-8'));
-   # print(bytes(ujson.dumps(broker_package), 'utf-8'))
-   # print(ujson.dumps(broker_package))
-    #PedometerInstance.process_raw_data(gyro['x_dir'], gyro['y_dir'], gyro['z_dir'])
-    #print(ujson.dumps(gyro))
-    #HeartRateProcessor.tick()
+    process_input_data() # Take in the raw data from the sensors
 
+    HeartRateClass.process_raw_lux(lux_sensor['channel_1']) # Process the data in the heart rate class
+    PedometerInstance.process_raw_data(gyro['x_dir'], gyro['y_dir'], gyro['z_dir']) # Process the data in the pedometer class
+
+    broker_package['Steps_Taken'] = PedometerInstance.getSteps() # Package the steps taken
+    broker_package['Heart_Rate'] = HeartRateClass.getHeartRate() # Package the result heartrate
+    MQTTClientInstance.publish_data('topic/state', bytes(ujson.dumps(broker_package), 'utf-8')); # Publish the data to the server
+
+    # Duty counter for the LED
     if (counter < duty_counter):
         counter += 1;
 
@@ -128,24 +137,18 @@ while(True):
         counter = 0;
         if (led_switch == 0):
             led_switch = 1;
-            LED_out.on()
+            LED_out.on() # Switches the LED on
         else:
             led_switch = 0;
-            LED_out.off()
+            LED_out.off() # Switches the LED off
 
 
-   # print(counter, duty_counter, duty_time, led_switch)
     utime.sleep_ms(delay) # 20 readings in a second
-    time += delay
+    time += delay # Elapsed time
 
     if (time % 1000 == 0):
-        print("Steps Taken:", PedometerInstance.getSteps())
+        print("Steps Taken:", PedometerInstance.getSteps()) ## Every second print the steps taken
         
         if (time % 5000 == 0):
-            print("Elapsed Time: ", time/1000)
+            print("Elapsed Time: ", time/1000) # Every 5 seconds print the elapsed time
 
-    #print(PedometerInstance.getSteps())
-    #print(ujson.dumps(gyro))
-    #HeartRateProcessor.tick()
-    #utime.sleep_ms(20) # 20 readings in a second
-    #Stop = PedometerInstance.WalkTenSteps()
